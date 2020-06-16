b61d5f4aef29d2bb4b2be4cba0ffb3999fc75271
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-17 00:36:19 +0500

    updated

--- project/src/Test.java
+++ project/src/Test.java
@@ -1,5 +1,7 @@
+package test;
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("OKKKKKKKKKKKKKKKKKKKKKk");
+		String name = "Oqba amir";
+		System.out.println(name.contains("ba"));
 	}
 }
