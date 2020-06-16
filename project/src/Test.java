0585ec9b8f9794704e18902752cb9c95512bd708
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-17 00:45:54 +0500

    updated

--- project/src/Test.java
+++ project/src/Test.java
@@ -2,13 +2,16 @@ public class Test {
 	public static void main(String[] args) {
 		String name = "Oqba amir";
 		if (name.contains("ba")){
-			System.out.println("Yes, <ba> in <" + name + ">");
+			System.out.println("<ba> in <" + name + ">");
 		}
 		if (name.endsWith("mir")){
-			System.out.println("Yes, <" + name + "> ends with  <mir>");
+			System.out.println("<" + name + "> ends with  <mir>");
 		}
 		if (name.equals("Oqba amir")){
-			System.out.println("Yes, the variabel <name> equals <Oqba amir>");
+			System.out.println("the variabel <name> equals <Oqba amir>");
+		}
+		if (name.equalsIgnoreCase("oqba AmiR")){
+			System.out.println("<" + name + "> is equal to <oqba AmiR> regardless of case");
 		}
 	}
 }
