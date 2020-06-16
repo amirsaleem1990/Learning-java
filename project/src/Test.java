cf806c034dd5c54639ec141733f8b4975e2b1b54
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-17 00:35:12 +0500

    updated

--- project/src/Test.java
+++ project/src/Test.java
@@ -1,6 +1,5 @@
-package test;
-pcublic class Test{
-	public static void main(String[] args){
-		String name = "Oqba amir";
-		System.out.println(name.contains("ba"));
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("OKKKKKKKKKKKKKKKKKKKKKk");
 	}
+}
