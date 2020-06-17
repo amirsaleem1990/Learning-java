3fe953b01df72c0518f2edbe20be0f2eb23ad7f4
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-17 20:30:51 +0500

    updated

--- project/src/Test.java
+++ project/src/Test.java
@@ -2,10 +2,10 @@ public class Test {
 	public static void main(String[] args){
 
 		// when constructer has no perametsr
-		// class_with_cunstructer obJ = new class_with_cunstructer();
+		class_with_cunstructer obJ = new class_with_cunstructer();
 
 		// when constructer has no perametsr
-		class_with_cunstructer obJ2 = new class_with_cunstructer_perameter("Amir", 30);
+		class_with_cunstructer_perameter obJ2 = new class_with_cunstructer_perameter("Amir", 30);
 		
 		// obJ.print_func();
 		obJ2.print_func();
