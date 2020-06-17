fc569701185c234581c8121ebb861994cb1c4df4
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-17 18:35:33 +0500

    updated

--- project/src/Test.java
+++ project/src/Test.java
@@ -1,5 +1,13 @@
 public class Test{
 	public static void main(String[] args){
-		
+		brother bro = new brother();
+		String brother_name = bro.name;
+		int brother_age = bro.age;
+
+		father fath = new father();
+		String father_name = fath.name;
+		int father_age = fath.age;
+
+		System.out.println("My Father's name is: " + father_name, ", and  he is " + father_age + " old\nMy brother's name is: " + brother_name, ", and he is " + brother_age + " old\n") 
 	}
 }
