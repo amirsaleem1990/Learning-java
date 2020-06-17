5f2c46655ea145e4c2fa2b4b87f0a11f06c97f30
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-17 19:15:20 +0500

    updated

---
+++ project/src/class_with_cunstructer.java
@@ -0,0 +1,13 @@
+public class class_with_cunstructer(){
+
+	String name = "Amir";
+	int age = 30;
+	public class_with_cunstructer(){
+		name = "Oqba";
+		age = 2; 
+	}
+
+	public void print_func(){
+		System.out.println("Name = " + name + "\nAge = " + age);
+	}
+}
