c62614b3695918014d72a691839496f510cdf942
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-17 19:15:58 +0500

    updated

--- project/src/Test.java
+++ project/src/Test.java
@@ -1,20 +1,6 @@
 public class Test {
 	public static void main(String[] args){
-
-	}
-}
-
-
-public class class_with_cunstructer(){
-
-	String name = "Amir";
-	int age = 30;
-	public class_with_cunstructer(){
-		name = "Oqba";
-		age = 2; 
-	}
-
-	public void print_func(){
-		System.out.println("Name = " + name + "\nAge = " + age);
+		class_with_cunstructer obJ = new class_with_cunstructer();
+		obJ.print_func();
 	}
 }
