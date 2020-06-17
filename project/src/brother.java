34f6828c036fadb1dc5f4fd682e6d6f28a62d1de
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-17 18:41:06 +0500

    updated

--- project/src/brother.java
+++ project/src/brother.java
@@ -2,7 +2,7 @@ public class brother {
 	String name = "Danish";
 	int age = 26;
 
-		public void print_(){
-		System.out.println("Method <print_> called from class <father>");
+	public void print_(){
+		System.out.println("Method <print_> called from class <brother>");
 	}
 }
