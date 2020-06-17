d302d94964924d80e0292b91e4c4e969099fda0e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-17 18:26:57 +0500

    updated

--- Test.java
+++ Test.java
@@ -1,5 +1,16 @@
 public class Test{
-	public static void main(String[] args){
-		System.out.println("OK");
+	public static void main(String[] args) {
+
+		void_func();
+		
+		int int_function_returned_value = int_func();
+		System.out.println("int_func returned value is: " + int_function_returned_value);
 	}
-}
+
+	public static void void_func() {
+		System.out.println("Function Called");
+	}
+	
+	public static int int_func() {
+		return 5;
+	}
