dd74783eb522529a61f03df23ec934c253d28a26
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-20 01:17:30 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/21-Polymorphism/Test.java
+++ 1-Fast-App-Development-YOUTUBE/21-Polymorphism/Test.java
@@ -1,3 +1,6 @@
+// https://youtu.be/yFBHsptsdOM
+// Polymorphism
+// Polymorphism ye hota h k ham kafi sary functions banaty hen same name k, to python me to ye hota h k koi error nahi aata balky last wala sab ko overrie kar deta h, or srif wohi accessable rehta h, java me esa nahi h, ham 1 hi name k multiple functions bana sakty hen bash shart ye h k si bhi function k arguments same na hon, yani k agar me ny <print_> k name sy 1 function banaya h jis me 1 hi perameter h jis ki type <int> h to ab me isi name <print_> ka koi dusra function bhi define kar sakta hun magar shart ye h k us ka perameter 1 hi int na hon, balky ya to 2 int peremeters hon, ye 1 int ho dusra koi or type ka, ye sry sy perameter hi na ho, to khulasa ye k perameters agar same na hon to dusra function banaya ja sakta h
 public class Test extends parent_class {
 	public static void main(String[] args) {
 
