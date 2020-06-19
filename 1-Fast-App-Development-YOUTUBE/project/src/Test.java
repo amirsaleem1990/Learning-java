264b83801fae239c26bf92c54977d00135be17fa
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-20 00:54:02 +0500

    updated

---
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+	public static void main(String[] args) {
+
+	}
+	public void print_() {
+		System.out.println("Test func 1 called");
+	}
+	public void print_(int x) {
+		System.out.println("Test func 2 called");
+	}
+	public void print_(int x, int i) {
+		System.out.println("Test func 3 called");
+	}
+	public void print_(String) {
+		System.out.println("Test func 4 called");
+	}
+}
