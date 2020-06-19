8c0802d059edeac50604dd3b62dbfaf114d3306b
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-20 01:10:09 +0500

    updated

---
+++ 1-Fast-App-Development-YOUTUBE/21-Polymorphism/Test.java
@@ -0,0 +1,23 @@
+public class Test extends parent_class {
+	public static void main(String[] args) {
+
+	Test test = new Test();
+	test.print_();
+	test.print_(5);
+	test.print_(5,6);
+	test.print_("Amir");
+	test.print_(4, "Amir");
+	}
+	public void print_() {
+		System.out.println("Test func 1 called, no perameter");
+	}
+	public void print_(int x) {
+		System.out.println("Test func 2 called, 1 int perameter");
+	}
+	public void print_(int x, int i) {
+		System.out.println("Test func 3 called, 2 int perameters");
+	}
+	public void print_(String x) {
+		System.out.println("Test func 4 called, 1 string perameter");
+	}
+}
