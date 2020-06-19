77163ec1cd1f3dbe039f04c522a3f712ae7f2dcd
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-20 01:04:32 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/parent_class.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/parent_class.java
@@ -2,8 +2,4 @@ public class parent_class {
 	public void print_(int x, String j) {
 		System.out.println("parent_class func 1 called");
 	}
-	public int print_(int x) {
-		System.out.println("parent_class func 2 called");
-		return 5;
-	}
 }
