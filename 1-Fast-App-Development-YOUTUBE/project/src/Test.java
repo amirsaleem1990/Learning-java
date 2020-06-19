ce68d109d74692b9e127a5cf784f6057463adf8c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-20 00:58:56 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -7,7 +7,7 @@ public class Test extends parent_class {
 	test.print_(5,6);
 	test.print_("Amir");
 	test.print_(4, "Amir");
-	test.print_(4)
+	test.print_(4);
 	}
 	public void print_() {
 		System.out.println("Test func 1 called");
@@ -18,7 +18,7 @@ public class Test extends parent_class {
 	public void print_(int x, int i) {
 		System.out.println("Test func 3 called");
 	}
-	public void print_(String) {
+	public void print_(String x) {
 		System.out.println("Test func 4 called");
 	}
 }
