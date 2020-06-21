20f69c4652f8720660f79784c69dd2b9cbbb57e5
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-21 21:30:56 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class Test extends classes {
 	public static void main(String[] args) {
 		Test t = new Test();
-		System.out.println("Before set new value: " + t.get_());
+		System.out.println("____ Before set a  new value ____\nmobile_no = " + t.get_());
 
-		System.out.print("\nPlease set new value to mobile_no: ");
+		System.out.print("\nPlease set a new value to mobile_no: ");
 		Scanner obj = new Scanner(System.in);
 		int x = obj.nextInt();
 		t.set_(x);
-		System.out.println("After set new value: " + t.get_());
+		System.out.println("____ After set a new value ____\nmobile_no = " + t.get_());
 	}
 }
