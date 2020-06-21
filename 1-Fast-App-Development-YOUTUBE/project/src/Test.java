45530bfd2aaa94498615e828064ce4558eaedc2f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-21 21:40:24 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,5 +1,6 @@
 // Encapsulation
 // https://youtu.be/Wo4Dx1pTWMY
+// Encapsulation: is me ham koi ksi class me koi variable banaty hen or usy public k bajary <private> kar daty hen, esa karny sy wo variable srif usi class me access ho sakta h, ab agar hame usy bahir sy bhi use karwana (eg: hame us ki value chahye bhi, or hame us value ko change bhi karna h), to ham usi variable ki class me 2 functions banatay hen <public> type k, chuky in functions ko wo variable accessable hota h (q k un ki or variable ki class 1 hi h) to ham apny kam un functions k zarye sy karwaty hen
 import java.util.Scanner;
 public class Test extends classes {
 	public static void main(String[] args) {
