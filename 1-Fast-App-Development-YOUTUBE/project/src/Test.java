64101e5c2947b673c2e45c522b4e133e26254f38
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-21 21:25:44 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -3,6 +3,9 @@
 public class Test extends classes {
 	public static void main(String[] args) {
 		Test t = new Test();
-		System.out.println(t.get_());
+		System.out.println("Before set new value: " + t.get_());
+
+		t.set_(987);
+		System.out.println("After set new value: " + t.get_());
 	}
 }
