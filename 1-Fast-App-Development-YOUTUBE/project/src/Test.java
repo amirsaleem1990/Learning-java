8d2c4bf046c6cbadd0c30087ecff69de1be1424f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-21 21:27:21 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,9 +1,12 @@
 // Encapsulation
 // https://youtu.be/Wo4Dx1pTWMY
+import java.util.Scanner;
 public class Test extends classes {
 	public static void main(String[] args) {
 		Test t = new Test();
 		System.out.println("Before set new value: " + t.get_());
+
+		Scanner obj = new Scanner(System.in);
 
 		t.set_(987);
 		System.out.println("After set new value: " + t.get_());
