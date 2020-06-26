0e30dda7b6e199eaee02ffff73f27594937b30b1
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-26 23:55:30 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args){
 		JFrame f = new JFrame("My first Java GUI program");
 		f.setSize(600,600);
+		f.setLocationRelativeTo(null);
 		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		f.setVisible(true);
 
