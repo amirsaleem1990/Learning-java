6ee378a0a274563b6507badfdab86c8b95605f9a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-26 23:53:59 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args){
 		JFrame f = new JFrame();
 		f.setSize(600,600);
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		f.setVisible(true);
+		
 	}
 }
