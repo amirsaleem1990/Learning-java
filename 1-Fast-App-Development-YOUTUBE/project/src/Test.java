865c2d26959e56b0743e63376875d0ec0f358799
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-26 23:57:33 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,9 +1,14 @@
 // Graphical User Interface
 import javax.swing.JFrame;
+import javax.swing.JButton;
 public class Test {
 	public static void main(String[] args){
 		JFrame f = new JFrame("My first Java GUI program");
 		f.setSize(600,600);
+
+		JButton b1 = new JButton();
+
+
 		f.setLocationRelativeTo(null);
 		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		f.setVisible(true);
