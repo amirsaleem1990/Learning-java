e089cf9741b2ed0c1c256f4a8fd1166ea3a8b202
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-26 23:58:19 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -6,7 +6,10 @@ public class Test {
 		JFrame f = new JFrame("My first Java GUI program");
 		f.setSize(600,600);
 
-		JButton b1 = new JButton();
+		JButton b1 = new JButton("Show Text");
+		JButton b2 = new JButton("Clear Text");
+
+
 
 
 		f.setLocationRelativeTo(null);
