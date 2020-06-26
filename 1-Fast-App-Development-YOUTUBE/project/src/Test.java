01b38f536606438833f1076c9ff9ae9132efad83
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 02:08:15 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -3,37 +3,32 @@ import java.awt.event.*;
 
 public class Test extends JFrame{
 
-public static void main(String[] args) {
-new Clickprogram();
-}
-    
-private JButton button1;
-    
-public Clickprogram()
-{
-       
-this.setSize(300,150);
-this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-this.setTitle("Click Event");
-        
-Clicklistener click= new Clicklistener();
-        
-JPanel panel1= new JPanel();
-button1= new JButton ("Click Me!");
-button1.addActionListener(click);
-panel1.add(button1);
-this.add(panel1);
-this.setVisible(true);
-}
-    
-private class Clicklistener implements ActionListener
-{
-public void actionPerformed(ActionEvent e)
-{
-if (e.getSource() == button1)
-{
-button1.setText("The button has been clicked");
-}
-}
-}
+	public static void main(String[] args) {
+		new Clickprogram();
+	}
+	    
+	JButton button1;
+	    
+	public void  Clickprogram(){
+		this.setSize(300,150);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Click Event");
+		        
+		Clicklistener click= new Clicklistener();
+		        
+		JPanel panel1= new JPanel();
+		button1= new JButton ("Click Me!");
+		button1.addActionListener(click);
+		panel1.add(button1);
+		this.add(panel1);
+		this.setVisible(true);
+	}
+	    
+	private class Clicklistener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+		if (e.getSource() == button1){
+			button1.setText("The button has been clicked");
+		}
+		}
+	}
 }
