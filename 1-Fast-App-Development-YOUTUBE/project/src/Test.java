ab3fce2900ce06cacf80531bc4d4324135f2f785
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 17:07:01 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -64,13 +64,13 @@ public class Test {
 				if (array[x].equals(Character.toString('/'))) {
 					double o1 = Double.parseDouble(array[x-1]);
 					double o2 = Double.parseDouble(array[x+1]);
-					// System.out.println(x);
-					// System.out.println(list);
-					// list.remove(x);
-					// list.remove(x);
-					// list.remove(x-1);
-					// System.out.println(list);
-					// System.out.println("\n\n");
+					System.out.println(x);
+					System.out.println(list);
+					list.remove(x);
+					list.remove(x);
+					list.remove(x-1);
+					System.out.println(list);
+					System.out.println("\n\n");
 					// System.out.println(o1 + ", " + o2);
 					sum += o1  / o2;
 				}else if(array[x].equals(Character.toString('*'))){
