6ff303c0407546f7d96b79dd77089125088c380b
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 13:04:45 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,20 +1,12 @@
-// import javax.script.ScriptEngineManager;
-// import javax.script.ScriptEngine;
-// import javax.script.ScriptException;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
 
-// public class Test {
-//   public static void main(String[] args) throws ScriptException {
-//     ScriptEngineManager mgr = new ScriptEngineManager();
-//     ScriptEngine engine = mgr.getEngineByName("JavaScript");
-//     String ans = "5/2*3";
-//     System.out.println(engine.eval(ans));
-//     } 
-// }
-import mXparser.*
-public class Test{
-	public static void main(String[] args){
-		Expression ans = new Expression("5/2*3");
-		double v = ans.calculate();
-		System.out.println(v);
-	}
+public class Test {
+  public static void main(String[] args) throws ScriptException {
+    ScriptEngineManager mgr = new ScriptEngineManager();
+    ScriptEngine engine = mgr.getEngineByName("JavaScript");
+    String ans = "5/2*3";
+    System.out.println(engine.eval(ans));
+    } 
 }
