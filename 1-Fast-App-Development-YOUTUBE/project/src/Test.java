753d903dcf5d5bb07be23eb6d79f0b5f2287f0d8
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 16:23:52 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -69,8 +69,9 @@ public class Test {
 		for(int i=0; i<array_2.length; i++){
 			int x = array_2[i];
 			if (x>0){
+				System.out.println(list[x-1] + "," + list[x] + "," + list[x+1]);
 				// list.remove(x-1);
-				list.remove(x);
+				// list.remove(x);
 				// list.remove(x+1);
 			}
 		}
