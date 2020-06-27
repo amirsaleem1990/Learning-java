c63ff78308626eff8e8fcd47f02a392fa0e5d70a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 17:39:48 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -72,7 +72,7 @@ public class Test {
 					list.remove(x-removed);
 					list.remove(x-removed);
 					list.remove(x-1-removed);
-					list.add(x-removed, sum);
+					list.add(x-removed-1, sum);
 					removed += 3;
 					System.out.println(list);
 					System.out.println("\n\n");
@@ -87,7 +87,7 @@ public class Test {
 					list.remove(x-removed);
 					list.remove(x-removed);
 					removed += 3;
-					list.add(x-removed, sum);
+					list.add(x-removed-1, sum);
 					System.out.println(list);
 					System.out.println("\n\n");
 					// System.out.println(o1 + ", " + o2);
