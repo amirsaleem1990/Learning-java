6207f3348f359cd964c3d31ad19373df31d1d37b
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 22:16:52 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -8,43 +8,6 @@ import java.util.Scanner;
 
 public class Test{
 	public static void main(String[] args){
-		String line;
-		Scanner scan = new Scanner(System.in);
-
-		ProcessBuilder builder = new ProcessBuilder("/home/amir/py 5+6");
-		builder.redirectErrorStream(true);
-		Process process = builder.start();
-
-
-		OutputStream stdin = process.getOutputStream ();
-		InputStream stderr = process.getErrorStream ();
-		InputStream stdout = process.getInputStream ();
-
-		BufferedReader reader = new BufferedReader (new InputStreamReader(stdout));
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-
-		String input = scan.nextLine();
-		input += "\n";
-		writer.write(input);
-		writer.flush();
-
-		input = scan.nextLine();
-		input += "\n";
-		writer.write(input);
-		writer.flush();
-
-		while ((line = reader.readLine ()) != null) {
-		System.out.println ("Stdout: " + line);
-		}
-
-		input = scan.nextLine();
-		input += "\n";
-		writer.write(input);
-		writer.close();
-
-		while ((line = reader.readLine ()) != null) {
-		System.out.println ("Stdout: " + line);
-		}
-		
+		Process p = Runtime.getRuntim().exec("command goes here");
 	}
 }
