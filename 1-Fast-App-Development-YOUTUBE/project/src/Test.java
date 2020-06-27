5c8d61693fb3492641e7755690e52c00fa826e24
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 17:35:21 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -61,6 +61,8 @@ public class Test {
 			double sum = 0;
 			int x = array_2[i];
 			if (x > 0){
+				System.out.println("x: " + x);
+				System.out.println("removed: " + removed);
 				if (array[x].equals(Character.toString('/'))) {
 					double o1 = Double.parseDouble(array[x-1]);
 					double o2 = Double.parseDouble(array[x+1]);
