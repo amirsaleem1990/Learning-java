f7e0ea28bb2430209273a9bedcfc313a9dfc990c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 22:49:22 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -72,23 +72,23 @@ public class Test{
 		
 		tf.setBounds      (30,  100, 580, 90);
 
-		b_add.setBounds   (30,  200, 90, 90); 
-		b_minus.setBounds (125, 200, 90, 90); 
-		b_devide.setBounds(220, 200, 90, 90); 
-		b_mult.setBounds  (315, 200, 90, 90); 
-		b_ans.setBounds   (410, 200, 100, 90);
-		b_clear.setBounds (515, 200, 90, 90);
+		b_add.setBounds   (30,  300, 90, 90); 
+		b_minus.setBounds (125, 300, 90, 90); 
+		b_devide.setBounds(220, 300, 90, 90); 
+		b_mult.setBounds  (315, 300, 90, 90); 
+		b_ans.setBounds   (410, 300, 100, 90);
+		b_clear.setBounds (515, 300, 90, 90);
 
-		b1.setBounds      (30,  300, 180, 90);
-		b2.setBounds      (230, 300, 180, 90);
-		b3.setBounds      (430, 300, 180, 90);
-		b4.setBounds      (30,  400, 180, 90);
-		b5.setBounds      (230, 400, 180, 90);
-		b6.setBounds      (430, 400, 180, 90);
-		b7.setBounds      (30,  500, 180, 90);
-		b8.setBounds      (230, 500, 180, 90);
-		b9.setBounds      (430, 500, 180, 90);
-		b0.setBounds      (30,  600, 580, 90);
+		b1.setBounds      (30,  400, 180, 90);
+		b2.setBounds      (230, 400, 180, 90);
+		b3.setBounds      (430, 400, 180, 90);
+		b4.setBounds      (30,  500, 180, 90);
+		b5.setBounds      (230, 500, 180, 90);
+		b6.setBounds      (430, 500, 180, 90);
+		b7.setBounds      (30,  600, 180, 90);
+		b8.setBounds      (230, 600, 180, 90);
+		b9.setBounds      (430, 600, 180, 90);
+		b0.setBounds      (30,  700, 580, 90);
 
 		b1.addActionListener(new ActionListener() {			
 			@Override
