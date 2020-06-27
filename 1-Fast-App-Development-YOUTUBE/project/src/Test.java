f7185822794ffa2c39c618f3989e2163726cd925
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 20:13:34 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -194,7 +194,7 @@ public class Test{
 				String ans = tf.getText();
 				String ans_2 = String.valueOf(eval_math_expression(ans));
 				if (ans_2.equals("0.8")){
-					tf.setText("my sweety AYESHOO~ ........ I LOVE U");
+					tf.setText("my sweety AYESHOO!  I LOVE U");
 				}else{
 					tf.setText(ans + " = " + ans_2);
 				}
