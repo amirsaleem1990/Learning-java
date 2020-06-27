222939e03cec22e4542e5a5c11455caa16915258
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 16:24:37 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -46,35 +46,36 @@ public class Test {
 		// List<String> list_ = Arrays.asList(array);
 
 		List list = new ArrayList(Arrays.asList(array));
+		System.out.println(list);
 
-		double sum = 0;
+		// double sum = 0;
 
-		for (int i=0; i<array_2.length; i++){
-			int x = array_2[i];
-			if (x > 0){
-				if (array[x].equals(Character.toString('/'))) {
-					double o1 = Double.parseDouble(array[x-1]);
-					double o2 = Double.parseDouble(array[x+1]);
-					// System.out.println(o1 + ", " + o2);
-					sum += o1  / o2;
-				}else if(array[x].equals(Character.toString('*'))){
-					double o1 = Double.parseDouble(array[x-1]);
-					double o2 = Double.parseDouble(array[x+1]);
-					sum += o1  * o2;
-					// System.out.println(o1 + ", " + o2);
-				}
-			}
-		}
+		// for (int i=0; i<array_2.length; i++){
+		// 	int x = array_2[i];
+		// 	if (x > 0){
+		// 		if (array[x].equals(Character.toString('/'))) {
+		// 			double o1 = Double.parseDouble(array[x-1]);
+		// 			double o2 = Double.parseDouble(array[x+1]);
+		// 			// System.out.println(o1 + ", " + o2);
+		// 			sum += o1  / o2;
+		// 		}else if(array[x].equals(Character.toString('*'))){
+		// 			double o1 = Double.parseDouble(array[x-1]);
+		// 			double o2 = Double.parseDouble(array[x+1]);
+		// 			sum += o1  * o2;
+		// 			// System.out.println(o1 + ", " + o2);
+		// 		}
+		// 	}
+		// }
 
-		for(int i=0; i<array_2.length; i++){
-			int x = array_2[i];
-			if (x>0){
-				System.out.println(list[x-1] + "," + list[x] + "," + list[x+1]);
-				// list.remove(x-1);
-				// list.remove(x);
-				// list.remove(x+1);
-			}
-		}
+		// for(int i=0; i<array_2.length; i++){
+		// 	int x = array_2[i];
+		// 	if (x>0){
+		// 		System.out.println(list[x-1] + "," + list[x] + "," + list[x+1]);
+		// 		// list.remove(x-1);
+		// 		// list.remove(x);
+		// 		// list.remove(x+1);
+		// 	}
+		// }
 		// System.out.println(list);
 
 
