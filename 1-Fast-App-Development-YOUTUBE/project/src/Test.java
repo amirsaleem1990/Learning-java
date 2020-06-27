265808b7089459b8432cbc7c82151b9287d99eeb
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 16:50:42 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -63,9 +63,10 @@ public class Test {
 				if (array[x].equals(Character.toString('/'))) {
 					double o1 = Double.parseDouble(array[x-1]);
 					double o2 = Double.parseDouble(array[x+1]);
-					list.remove(x-1);
-					list.remove(x+1);
 					list.remove(x);
+					list.remove(x);
+					// list.remove(x-1);
+					// list.remove(x+1);
 					// list.remove(x+1);
 					// System.out.println(o1 + ", " + o2);
 					sum += o1  / o2;
