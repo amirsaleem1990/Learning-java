9b1247d691e48e28bdede582fcd41ca46481ce7c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 12:57:30 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,9 +1,9 @@
 public class Test{
 	public static void main(String[] args){
 		String ans = "5/2*3";
-		// for (int i=0; i<ans.length(); i++){
-			// System.out.println(ans[i]);
-		// }
-		System.out.println(ans[2]);
+		for (int i=0; i<ans.length(); i++){
+			System.out.println(ans.charAt(i));
+		}
+		// System.out.println(ans[2]);
 	}
 }
