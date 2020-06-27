9951156826c2b87814254464830a492af9946bc3
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 15:29:34 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -34,7 +34,7 @@ public class Test {
 		int array_2_filled = 0;
 		int[] array_2 = new int[5]; 
 		for (int i=1; i<array.length; i=i+2){
-			// System.out.println(array[i]);
+			System.out.println(array[i]);
 			if (array[i].equals(Character.toString('/')) || 
 				array[i].equals(Character.toString('*'))){
 					array_2[array_2_filled] = i;
