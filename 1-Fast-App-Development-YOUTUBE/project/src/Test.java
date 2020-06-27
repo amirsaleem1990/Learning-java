28bf8c872442d105785ef86942e427294c63b02c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 22:19:23 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,13 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
-
 public class Test{
 	public static void main(String[] args){
-		Process p = Runtime.getRuntim().exec("/home/amir/py 5+6");
+		Runtime.getRuntim().exec("/home/amir/py 5+6");
 	}
 }
