a83aaffcc7bed6e75d4e04a60812ceecb21747a8
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 15:37:45 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -42,7 +42,8 @@ public class Test {
 			}
 		}
 		for (int i=0; i<array_2.length; i++){
-			System.out.println(array_2[i]);
+			int x = array_2[i];
+			System.out.println(array[x]);
 		}
 		// for (int i=1; i<array.length; i=i+2){
 		// 	// System.out.println(array[i]);
