ff595819bd59e7d5d73c67b435a2904fad2d91b4
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 22:27:08 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -4,8 +4,22 @@ import java.io.InputStreamReader;
 
 public class Test{
 	public static void main(String[] args){
-		String command = "/home/amir/py 5+6";
-	    Process process = Runtime.getRuntime().exec(command);
-	    System.out.println(process);
+		String command = "/home/amir/py 5";
+		 
+		try {
+		    Process process = Runtime.getRuntime().exec(command);
+		 
+		    BufferedReader reader = new BufferedReader(
+		            new InputStreamReader(process.getInputStream()));
+		    String line;
+		    while ((line = reader.readLine()) != null) {
+		        System.out.println(line);
+		    }
+		 
+		    reader.close();
+		 
+		} catch (IOException e) {
+		    e.printStackTrace();
 		}
 	}
+}
