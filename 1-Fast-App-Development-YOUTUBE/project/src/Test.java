785c6225a4237a1ccae077dcc888d863bd1c9362
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 16:19:09 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -52,12 +52,13 @@ public class Test {
 		for (int i=0; i<array_2.length; i++){
 			int x = array_2[i];
 			if (x > 0){
+				System.out.println(x);
 				if (array[x].equals(Character.toString('/'))) {
 					double o1 = Double.parseDouble(array[x-1]);
 					double o2 = Double.parseDouble(array[x+1]);
 					// System.out.println(o1 + ", " + o2);
 					sum += o1  / o2;
-					// list.remove(x);
+					list.remove(x);
 					// System.out.println(x);
 					// list.remove(x-1);
 					// list.remove(x+1);
@@ -72,7 +73,7 @@ public class Test {
 				}
 			}
 		}
-		System.out.println(list);
+		// System.out.println(list);
 
 
 		// for (int i=1; i<array.length; i=i+2){
