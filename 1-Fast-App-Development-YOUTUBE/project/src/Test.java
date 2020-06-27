10c0f6edc9992c2fd5fbdfe28fe3b2bc5cf5e0f5
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 21:04:13 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -3,18 +3,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class Test{
-	public static void main(args[] main){
-		System.out.println("OK");
-		// ProcessBuilder builder = new ProcessBuilder("~/home/amir/py 5+8");
-		// builder.redirectErrorStream(true);
-		// Process process = builder.start();
-		// InputStream is = process.getInputStream();
-		// BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+	public static void main(String[] args){
+		 ProcessBuilder builder = new ProcessBuilder("~/home/amir/py 5+8");
+		 builder.redirectErrorStream(true);
+		 Process process = builder.start();
+		 InputStream is = process.getInputStream();
+		 BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
-		// String line = null;
-		// while ((line = reader.readLine()) != null) {
-		   // System.out.println(line);
-		// }
+		 String line = null;
+		 while ((line = reader.readLine()) != null) {
+		    System.out.println(line);
+		 }
 
 	}
 }
