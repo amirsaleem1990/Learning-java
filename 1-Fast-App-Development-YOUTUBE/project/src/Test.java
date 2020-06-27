017f7e763179707fada629dc845c70a9d6eaefe8
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 15:45:54 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -48,6 +48,7 @@ public class Test {
 				if (array[x].equals(Character.toString('/'))) {
 					double o1 = Double.parseDouble(array[x-1]);
 					double o2 = Double.parseDouble(array[x+1]);
+					System.out.println(o1 + ", " + o2);
 					sum += o1  / o2;
 					System.out.println(sum);
 				}			
