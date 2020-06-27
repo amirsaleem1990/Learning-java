dc980b80441a89ac4f461800940f7b75f8bec1f7
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 13:14:01 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -178,7 +178,8 @@ public class Test {
 				String ans = tf.getText();
 			    ScriptEngineManager mgr = new ScriptEngineManager();
 			    ScriptEngine engine = mgr.getEngineByName("JavaScript"); 
-			    System.out.println(engine.eval(ans));
+			    System.out.println(ans.length());
+			    // System.out.println(engine.eval(ans));
 			    // double d = engine.eval(ans);
 			    // tf.setText(d);
 			    // System.out.println(d);	
