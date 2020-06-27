b78d4292485f00629efd92699e2b4c765fcc0be1
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 14:41:14 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,20 +1,18 @@
 public class Test {
 	public static void main(String[] args) {
-		String ans = "55/22+3/88";
+		String ans = "55/22+3/88+0-9";
 		// String[] array = new String[10];
 		// String temp_;
 		for (int i=0; i<ans.length(); i++) {
 			char character = ans.charAt(i);
-			System.out.println(character == '+' || character == '/');
-			System.out.println(character);
-			// if (character == "+"	 || 
-				// character == "-"	 ||
-				// character == "*"	 ||
-				// character == "/"	){
-					// System.out.println(ans.charAt(i));
+			if (character == '+'  || 
+				character == '-'  ||
+				character == '*'  ||
+				character == '/' ){
+					System.out.println(character);
 				// if (temp_.length() > 1){
 			// 		array[array.length+1] = temp_;
-				// }
+				}
 				// array[array.length+1] = (String) character;
 			// }else{
 			// 	temp_ += character;
