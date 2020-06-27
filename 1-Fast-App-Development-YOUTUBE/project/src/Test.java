7db4c2f64c511eba1c7259e9dd5fe466d91e2186
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 17:52:07 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -94,10 +94,7 @@ public class Test {
 				}
 			}
 		}
-		System.out.println(list);
-		for (int i=1; i<list.length(); i=i+2){
-			System.out.println(list);
-		}
+		System.out.println(list.length);
 		// System.out.println(sum);
 
 		// for(int i=0; i<array_2.length; i++){
