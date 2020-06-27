9c978e49d6e3a5a40728dbd1f26a76bb05df8bc6
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 14:52:54 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
 public class Test {
 	public static void main(String[] args) {
 		String ans = "55/22+3/88+0-9";
-		// String[] array = new String[10];
-		ArrayList<String> LIST = new ArrayList<>();
+		String[] array = new String[10];
 		String temp_ = "";
 		for (int i=0; i<ans.length(); i++) {
 			char character = ans.charAt(i);
@@ -13,12 +11,12 @@ public class Test {
 				character == '/' ){
 				if (temp_.length() > 1){
 					System.out.println("OK");
-					LIST.add(temp_);
+					array[array.length+1] = temp_;
 				}
-				LIST.add(character);
-			}else{
-				temp_ += character;
-			}
+				// array[array.length+1] = (String) character;
+			// }else{
+			// 	temp_ += character;
+			// }
 		}
 		// for (int i=0; i<array.length; i++){
 			// System.out.println(array[i]);
