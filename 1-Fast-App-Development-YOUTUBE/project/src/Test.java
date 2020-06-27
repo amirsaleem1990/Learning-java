a4270794b1d7860c4f8b2a60fc54aa4f3bd72283
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 15:01:20 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,27 +1,29 @@
 public class Test {
 	public static void main(String[] args) {
 		String ans = "55/22+3/88+0-9";
-		String[] array = new String[10];
-		System.out.println(array[0]);
-		// String temp_ = "";
-		// for (int i=0; i<ans.length(); i++) {
-		// 	char character = ans.charAt(i);
-		// 	if (character == '+'  || 
-		// 		character == '-'  ||
-		// 		character == '*'  ||
-		// 		character == '/' ){
-		// 		if (temp_.length() > 1){
-		// 			System.out.println("OK");
-		// 			System.out.println("_____" + array.length);
-		// 			array[array.length+1] = temp_;
-		// 		}
-		// 		array[array.length+1] = Character.toString(character);
-		// 	}
-		// 	else{
-		// 		temp_ += Character.toString(character);
-		// 		System.out.println(temp_);
-		// 	}
-		// }
+		String[] array = new String[15];
+		int arry_filled = 0;
+		String temp_ = "";
+		for (int i=0; i<ans.length(); i++) {
+			char character = ans.charAt(i);
+			if (character == '+'  || 
+				character == '-'  ||
+				character == '*'  ||
+				character == '/' ){
+				if (temp_.length() > 1){
+					System.out.println("OK");
+					System.out.println("_____" + array.length);
+					array[arry_filled] = temp_;
+					arry_filled += 1;
+				}
+				array[arry_filled] = Character.toString(character);
+				arry_filled += 1;
+			}
+			else{
+				temp_ += Character.toString(character);
+				System.out.println(temp_);
+			}
+		}
 		// for (int i=0; i<array.length; i++){
 			// System.out.println(array[i]);
 		// }
