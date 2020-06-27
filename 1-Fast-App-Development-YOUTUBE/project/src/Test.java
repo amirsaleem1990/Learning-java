48caab8d8c55c63db8e40bed6870f3b19dd9b2f7
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 22:23:31 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,5 +1,21 @@
 public class Test{
 	public static void main(String[] args){
-		Runtime.getRuntim().exec("/home/amir/py 5+6");
+		String command = "ping www.codejava.net";
+		 
+		try {
+		    Process process = Runtime.getRuntime().exec(command);
+		 
+		    BufferedReader reader = new BufferedReader(
+		            new InputStreamReader(process.getInputStream()));
+		    String line;
+		    while ((line = reader.readLine()) != null) {
+		        System.out.println(line);
+		    }
+		 
+		    reader.close();
+		 
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
 	}
 }
