959a90e85d57f4632e97c100a18f36f362811ded
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 13:05:35 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,9 +1,5 @@
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
-
 public class Test {
-  public static void main(String[] args) throws ScriptException {
+  public static void main(String[] args)  {
     ScriptEngineManager mgr = new ScriptEngineManager();
     ScriptEngine engine = mgr.getEngineByName("JavaScript");
     String ans = "5/2*3";
