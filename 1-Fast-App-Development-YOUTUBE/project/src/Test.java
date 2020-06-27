1915d737a91163ee081947093a99704b9c17a22d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 22:15:07 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -4,10 +4,43 @@ import java.io.InputStreamReader;
 
 public class Test{
 	public static void main(String[] args){
-//		Process process = Runtime.getRuntime ().exec ("/home/amir/py 5+6");
+		String line;
+		Scanner scan = new Scanner(System.in);
+
 		ProcessBuilder builder = new ProcessBuilder("/home/amir/py 5+6");
 		builder.redirectErrorStream(true);
-		Process builder.start();
+		Process process = builder.start();
+
+
+		OutputStream stdin = process.getOutputStream ();
+		InputStream stderr = process.getErrorStream ();
+		InputStream stdout = process.getInputStream ();
+
+		BufferedReader reader = new BufferedReader (new InputStreamReader(stdout));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+		String input = scan.nextLine();
+		input += "\n";
+		writer.write(input);
+		writer.flush();
+
+		input = scan.nextLine();
+		input += "\n";
+		writer.write(input);
+		writer.flush();
+
+		while ((line = reader.readLine ()) != null) {
+		System.out.println ("Stdout: " + line);
+		}
+
+		input = scan.nextLine();
+		input += "\n";
+		writer.write(input);
+		writer.close();
+
+		while ((line = reader.readLine ()) != null) {
+		System.out.println ("Stdout: " + line);
+		}
 		
 	}
 }
