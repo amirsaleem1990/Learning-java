f8178ec9edbbc4ccbcda7c6a149112ef6721a255
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 15:48:09 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -50,8 +50,12 @@ public class Test {
 					double o2 = Double.parseDouble(array[x+1]);
 					System.out.println(o1 + ", " + o2);
 					sum += o1  / o2;
-					System.out.println(sum);
-				}			
+				}else if(array[x].equals(Character.toString('*'))){
+					double o1 = Double.parseDouble(array[x-1]);
+					double o2 = Double.parseDouble(array[x+1]);
+					System.out.println(o1 + ", " + o2);
+					sum += o1  * o2;
+				}
 			}
 		}
 		// for (int i=1; i<array.length; i=i+2){
