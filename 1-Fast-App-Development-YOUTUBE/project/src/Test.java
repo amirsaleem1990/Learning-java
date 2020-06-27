e208c5ff98e7d571d262e0e529669706c492decf
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-27 16:02:32 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -43,26 +43,25 @@ public class Test {
 		}
 
 		List<String> list = Arrays.asList(array);
-		System.out.println(list);
 
-		// double sum = 0;
+		double sum = 0;
 
-		// for (int i=0; i<array_2.length; i++){
-		// 	int x = array_2[i];
-		// 	if (x > 0){
-		// 		if (array[x].equals(Character.toString('/'))) {
-		// 			double o1 = Double.parseDouble(array[x-1]);
-		// 			double o2 = Double.parseDouble(array[x+1]);
-		// 			// System.out.println(o1 + ", " + o2);
-		// 			sum += o1  / o2;
-		// 		}else if(array[x].equals(Character.toString('*'))){
-		// 			double o1 = Double.parseDouble(array[x-1]);
-		// 			double o2 = Double.parseDouble(array[x+1]);
-		// 			sum += o1  * o2;
-		// 			// System.out.println(o1 + ", " + o2);
-		// 		}
-		// 	}
-		// }
+		for (int i=0; i<array_2.length; i++){
+			int x = array_2[i];
+			if (x > 0){
+				if (array[x].equals(Character.toString('/'))) {
+					double o1 = Double.parseDouble(array[x-1]);
+					double o2 = Double.parseDouble(array[x+1]);
+					// System.out.println(o1 + ", " + o2);
+					sum += o1  / o2;
+				}else if(array[x].equals(Character.toString('*'))){
+					double o1 = Double.parseDouble(array[x-1]);
+					double o2 = Double.parseDouble(array[x+1]);
+					sum += o1  * o2;
+					// System.out.println(o1 + ", " + o2);
+				}
+			}
+		}
 
 
 
