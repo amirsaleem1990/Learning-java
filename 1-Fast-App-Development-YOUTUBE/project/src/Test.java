bc03c07ab1e7afb22142bc87ec28a470ad9f4c96
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-28 13:01:12 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,15 +1,15 @@
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
-
 import java.util.Scanner;
+
 public class Test {
   public static void main(String[] args) throws ScriptException {
     ScriptEngineManager mgr = new ScriptEngineManager();
     ScriptEngine engine = mgr.getEngineByName("JavaScript");
 
     System.out.println("Enter your Expression: ");
-    Scanner value_ ; Scanner(System.in);
+    Scanner value_ = new Scanner(System.in);
     String expression = value_.nextInt();
     // String foo = "40/2+5";
     System.out.println(engine.eval(expression));
