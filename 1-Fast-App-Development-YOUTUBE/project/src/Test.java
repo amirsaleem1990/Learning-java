46a815312cb04bd8084636f8fca090db28749c51
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-29 10:50:44 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,10 +1,16 @@
-import javax.script.*;
-
-public class Test {
-    public static void main(String[] args) throws ScriptException {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("js");
-        Bindings obj = (Bindings)engine.eval("var obj = { value: 1 };  obj; ");
-        Integer value = (Integer)obj.get("value");
-        System.out.println(value); // prints: 1
-    }
-}
+//"/home/amir/github/working/Namaz-times/Namaz_2.csv"
+import java.time.LocalDate;
+import java.io.*;  
+import java.util.Scanner;  
+public class Test  {
+	public static void main(String[] args) throws Exception {  
+		String date_ = LocalDate.now().toString();
+		Scanner sc = new Scanner(new File("/home/amir/github/working/Namaz-times/Namaz_2.csv"));  
+		sc.useDelimiter(",");
+		while (sc.hasNext()) {  //returns a boolean value
+			String am = sc.next();  //find and returns the next complete token from this scanner  
+			System.out.println(am.length();)
+		}
+	sc.close();
+	}  
+}  
