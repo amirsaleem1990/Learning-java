e40f16951cc09b7717fd351772edee76bd53c2e2
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-30 02:01:53 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,16 +1,22 @@
-import java.text.SimpleDateFormat;  
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date; 
 
 public class Test{
 	public static void main(String[] args) {
-		String name = FORLOOP();
-		System.out.println(name);
-		public String final FORLOOP() {
-			for (int i=0; i<4; i++) {
-				String name = "amir";
-				String fName = "saleem";
-				return name + "||" + fName;
-			}
-		}
+        String current_time = java.time.LocalTime.now().toString();
+//		SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+//		Date date = new Date();
+//		String current_time = formatter.format(date).toString();
+
+		String current_hour    = current_time.split(":")[0].replaceFirst("^0+(?!$)", "");
+		String current_minutes = current_time.split(":")[1].replaceFirst("^0+(?!$)", "");
+
+		int current_time_in_minutes = 0;
+
+		current_time_in_minutes += Integer.parseInt(current_hour)*60;
+		current_time_in_minutes += Integer.parseInt(current_minutes);
+
+	    System.out.println(current_time_in_minutes );
 	}
 }
