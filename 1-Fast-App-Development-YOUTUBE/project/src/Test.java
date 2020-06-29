c792541d599e4e850b6935b78c55f4c7dff33872
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-29 11:33:08 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,10 +1,12 @@
 //"/home/amir/github/working/Namaz-times/Namaz_2.csv"
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.io.*;  
 import java.util.Scanner;  
 public class Test  {
 	public static void main(String[] args) throws Exception {  
-		String date_ = LocalDate.now().toString();
+		String date_ = LocalDate.now().toString(); // 2020-06-29
+		String system_date = date_.split("-")[1] + "-" + date_.split("-")[2];
 		Scanner sc = new Scanner(new File("/home/amir/github/working/Namaz-times/Namaz_2.csv"));  
 		sc.useDelimiter("\n");
 //		Month,Date,Subah_sadiq,Tulu_aaftab,Zawal,Asr_1,Asr_2,Magrib,Isha
@@ -17,10 +19,12 @@ public class Test  {
 		String Asr_2;
 		String Magrib;
 		String Isha;
-		
+		// System.out.println(date_);
 		while (sc.hasNext()) {  //returns a boolean value
-			String[] x = sc.next().split(",");  //find and returns the next complete token from this scanner  
-			System.out.println(x[1]);
+			String row = sc.next(); // 8,31,04:54,06:12,12:32,16:01,17:04,18:53,20:10
+			String[] arrayrow = row.split(","); 
+			String csv_date = arrayrow[0] + "-" + arrayrow[1];
+			System.out.println("csv_date: " + csv_date +  "\tsystem date: " + system_date);
 		}
 	sc.close();
 	}  
