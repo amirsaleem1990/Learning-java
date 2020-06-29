38f0166deec6dd0feb44026c6c73c437000f5c2d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-29 13:28:10 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -5,56 +5,101 @@ import java.io.*;
 import java.util.Scanner;  
 public class Test  {
 	public static void main(String[] args) throws Exception {
-	    String current_time = java.time.LocalTime.now().toString();
-	    System.out.println(current_time);  
-// 		String date_ = LocalDate.now().toString(); // 2020-06-29
-// 		String current_month = date_.split("-")[1];
-// 		String current_date  = date_.split("-")[2];
-// 		current_month = current_month.replaceFirst("^0+(?!$)", "");
-// 		current_date  = current_date.replaceFirst ("^0+(?!$)", "");
-// 		String system_date = current_month + "-" + current_date;
-// 		Scanner sc = new Scanner(new File("/home/amir/github/working/Namaz-times/Namaz_2.csv"));  
-// 		sc.useDelimiter("\n");
-// //		Month,Date,Subah_sadiq,Tulu_aaftab,Zawal,Asr_1,Asr_2,Magrib,Isha
-// 		while (sc.hasNext()) {  //returns a boolean value
-// 			String row = sc.next(); // 8,31,04:54,06:12,12:32,16:01,17:04,18:53,20:10
-// 			String[] arrayrow = row.split(","); 
-// 			String csv_date = arrayrow[0] + "-" + arrayrow[1];
-// 			if (csv_date.equals(system_date)){
-// 			// 	String month = arrayrow[0];
-// 			// 	String date = arrayrow[1];
-// 			// 	String Subah_sadiq = arrayrow[2];
-// 			// 	String Tulu_aaftab = arrayrow[3];
-// 			// 	String Zawal = arrayrow[4];
-// 			// 	String Asr_1 = arrayrow[5];
-// 			// 	String Asr_2 = arrayrow[6];
-// 			// 	String Magrib = arrayrow[7];
-// 			// 	String Isha = arrayrow[8];	
-// 			// 	System.out.println("Subah_sadiq: " +  Subah_sadiq);
-// 			// 	System.out.println("Tulu_aaftab: " +  Tulu_aaftab);
-// 			// 	System.out.println("Zawal      : " +  Zawal);
-// 			// 	System.out.println("Asr_1      : " +  Asr_1);
-// 			// 	System.out.println("Asr_2      : " +  Asr_2);
-// 			// 	System.out.println("Magrib     : " +  Magrib);
-// 			// 	System.out.println("Isha       : " +  Isha);
-// 				int[] arrayrow_int = to_int(arrayrow);
-// 				for (int i=0; i<arrayrow_int.length; i++){
-// 					System.out.println(arrayrow_int[i]);
-// 				}
-// 			}
-// 		}
-// 	sc.close();
-// 	}  
-// 	public static int[] to_int(String[] x){
-// 		int[] array_minutes = new int[7];
-// 		for (int i=2; i<x.length; i++){
-// 			String[] splited = x[i].split(":");
-// 			String hour   = splited[0].replaceFirst("^0+(?!$)", "");
-// 			String minute = splited[1].replaceFirst("^0+(?!$)", "");  
-// 			int h_m = Integer.parseInt(hour)*60;
-// 			int m_m = Integer.parseInt(minute);
-// 			array_minutes[i-2] = h_m + m_m;
-// 		}
-// 		return array_minutes;
+		int    current_time = current_time();  // 781
+		String system_date  = current_date();  // 6-29
+
+		Scanner sc = new Scanner(new File("/home/amir/github/working/Namaz-times/Namaz_2.csv"));  
+		sc.useDelimiter("\n");
+//		Month,Date,Subah_sadiq,Tulu_aaftab,Zawal,Asr_1,Asr_2,Magrib,Isha
+		while (sc.hasNext()) {  //returns a boolean value
+			String row = sc.next(); // 8,31,04:54,06:12,12:32,16:01,17:04,18:53,20:10
+			String[] arrayrow = row.split(","); 
+			String csv_date = arrayrow[0] + "-" + arrayrow[1];
+
+			if (csv_date.equals(system_date)){
+				int[] arrayrow_int = to_int(arrayrow);
+				for (int i=0; i<arrayrow_int.length; i++){
+					if (arrayrow_int[i] > current_time){
+						int next_namaz_time_in_minute = arrayrow_int[i];
+						int minutes_baqi_hen = next_namaz_time_in_minute - current_time;
+
+						String next_namaz_name_ = next_namaz_name(i);
+
+						System.out.println(minutes_baqi_hen);
+						System.out.println(row.split(",")[i+2]);
+						System.out.println(next_namaz_name_);
+
+						break;
+					}
+				}
+			}
+		}
+	sc.close();
+	}  
+
+	// helper functions
+	public static int[] to_int(String[] x){
+		int[] array_minutes = new int[7];
+		for (int i=2; i<x.length; i++){
+			String[] splited = x[i].split(":");
+			String hour   = splited[0].replaceFirst("^0+(?!$)", "");
+			String minute = splited[1].replaceFirst("^0+(?!$)", "");  
+			int h_m = Integer.parseInt(hour)*60;
+			int m_m = Integer.parseInt(minute);
+			array_minutes[i-2] = h_m + m_m;
+		}
+		return array_minutes;
+	}
+	public static int current_time(){
+		String current_time = java.time.LocalTime.now().toString();
+
+		String current_hour    = current_time.split(":")[0].replaceFirst("^0+(?!$)", "");
+		String current_minutes = current_time.split(":")[1].replaceFirst("^0+(?!$)", "");
+
+		int current_time_in_minutes = 0;
+
+		current_time_in_minutes += Integer.parseInt(current_hour)*60;
+		current_time_in_minutes += Integer.parseInt(current_minutes);
+
+		return current_time_in_minutes;
+	}
+	public static String current_date(){
+		String date_ = LocalDate.now().toString(); // 2020-06-29
+
+		String current_month = date_.split("-")[1];
+		String current_date  = date_.split("-")[2];
+
+		current_month = current_month.replaceFirst("^0+(?!$)", "");
+		current_date  = current_date.replaceFirst ("^0+(?!$)", "");
+
+		String system_date = current_month + "-" + current_date;
+		return system_date;
+	}
+	public static String next_namaz_name(int i){
+		String name="";
+		switch (i){
+			case 0:
+				name =  "Subah_sadiq";
+				break;
+			case 1:
+				name =  "Tulu_aaftab";
+				break;
+			case 2:
+				name =  "Zawal";
+				break;
+			case 3:
+				name =  "Asr_1";
+				break;
+			case 4:
+				name =  "Asr_2";
+				break;
+			case 5:
+				name =  "Magrib";
+				break;
+			case 6:
+				name =  "Isha";
+				break;
+			}
+		return name;
 	}
 }
