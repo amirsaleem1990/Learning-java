daa3bebcadc3960b91080fd1732b487069d7ce79
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-29 11:10:51 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -6,10 +6,21 @@ public class Test  {
 	public static void main(String[] args) throws Exception {  
 		String date_ = LocalDate.now().toString();
 		Scanner sc = new Scanner(new File("/home/amir/github/working/Namaz-times/Namaz_2.csv"));  
-		sc.useDelimiter(",");
+		sc.useDelimiter("\n");
+//		Month,Date,Subah_sadiq,Tulu_aaftab,Zawal,Asr_1,Asr_2,Magrib,Isha
+		String month;
+		String date;
+		String Subah_sadiq;
+		String Tulu_aaftab;
+		String Zawal;
+		String Asr_1;
+		String Asr_2;
+		String Magrib;
+		String Isha;
+		
 		while (sc.hasNext()) {  //returns a boolean value
-			String am = sc.next();  //find and returns the next complete token from this scanner  
-			System.out.println(am.length();)
+			String[] x = sc.next().split(",");  //find and returns the next complete token from this scanner  
+			System.out.println(x[1]);
 		}
 	sc.close();
 	}  
