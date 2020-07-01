1ae611972665d81df3015412f4e59838eea8d8c3
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-07-01 15:25:28 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,22 +1,8 @@
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Date; 
-
 public class Test{
 	public static void main(String[] args) {
-        String current_time = java.time.LocalTime.now().toString();
-//		SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-//		Date date = new Date();
-//		String current_time = formatter.format(date).toString();
-
-		String current_hour    = current_time.split(":")[0].replaceFirst("^0+(?!$)", "");
-		String current_minutes = current_time.split(":")[1].replaceFirst("^0+(?!$)", "");
-
-		int current_time_in_minutes = 0;
-
-		current_time_in_minutes += Integer.parseInt(current_hour)*60;
-		current_time_in_minutes += Integer.parseInt(current_minutes);
-
-	    System.out.println(current_time_in_minutes );
-	}
+    	int x=119;
+	   	int h = x/60;
+	   	// int m = x & 60;
+    	System.out.println(x % 60);
+    }
 }
