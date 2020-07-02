bccaba0c5389f30285b818fff466083981aa8aaa
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-07-02 22:07:37 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,8 +1,7 @@
 public class Test{
 	public static void main(String[] args) {
-    	int x=119;
-	   	int h = x/60;
-	   	// int m = x & 60;
-    	System.out.println(x % 60);
+		int x , y, z;
+		x = y  = z = 99;
+		System.out.println(x + " " + y);
     }
 }
