1f1bd05316f82f8393613ce276d7996b9192b3b7
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-07-04 12:01:34 +0500

    updated

--- 1-Fast-App-Development-YOUTUBE/project/src/Test.java
+++ 1-Fast-App-Development-YOUTUBE/project/src/Test.java
@@ -1,11 +1,6 @@
 public class Test{
 	public static void main(String[] args){
-		DateTimeFormatter parser = ISODateTimeFormat.date();
-
-		DateTime date = parser.parseDateTime(dateString);
-
-		String nextDay = parser.print(date.plusDays(1));
-		System.out.println(nextDay);
+		System.out.println("amir");
 	}
 }
 
